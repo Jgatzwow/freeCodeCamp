c26c885dac0dfd153f05055cc42bf9953088ca76
Jgatzwow <thegreatgatsby1993@gmail.com>
2022-08-04 17:12:35 +0300

    Added file with React block Tasks;

---
+++ freeCodeCampReactTasks.js
@@ -0,0 +1,32 @@
+/* task 1*/
+const JSX = <h1>Hello JSX!</h1>;
+/* task 2*/
+const JSX = <div>
+    <h1>Hello JSX</h1>
+    <p>Yup Hello</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+</div>
+/* task 3*/
+const JSX = (
+    <div>
+        {/* some note*/}
+        <h1>This is a block of JSX</h1>
+        <p>Here's a subtitle</p>
+    </div>
+);
+/* task 4*/
+/* task 5*/
+/* task 6*/
+/* task 7*/
+/* task 8*/
+/* task 9*/
+/* task 10*/
+/* task 11*/
+/* task 12*/
+/* task 13*/
+/* task 14*/
+/* task 15*/
