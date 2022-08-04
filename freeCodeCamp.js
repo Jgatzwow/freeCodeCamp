14029446c1b11956dece2447febacc78756ad0ae
Jgatzwow <thegreatgatsby1993@gmail.com>
2022-08-04 08:39:45 +0300

    Added task 77

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -2,7 +2,7 @@
 function caseInSwitch(val) {
     let answer = "";
     // Only change code below this line
-    switch (val){
+    switch (val) {
         case 1:
             answer = "alpha"
             break;
@@ -22,3 +22,31 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1);
+
+/*task 77 - Selecting from Many Options with Switch Statements*/
+
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case "a":
+            answer = "apple"
+            break;
+        case "b":
+            answer = "bird"
+            break;
+        case "c":
+            answer = "cat"
+            break;
+        default:
+            answer = "stuff"
+            break;
+
+    }
+
+
+    // Only change code above this line
+    return answer;
+}
+
+switchOfStuff(1);
