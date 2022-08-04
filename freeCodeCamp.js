097ff15755dad1d81f1af6ec72cf97e13e3714fe
Jgatzwow <thegreatgatsby1993@gmail.com>
2022-08-04 13:24:45 +0300

    Added tasks 83 - 90

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -135,7 +135,6 @@ function abTest(a, b) {
 abTest(2, 2);
 
 /*task 82*/
-80
 let count = 0;
 
 function cc(card) {
@@ -146,7 +145,7 @@ function cc(card) {
         case 4:
         case 5:
         case 6:
-           count += 1
+            count += 1
             break;
         case 7:
         case 8:
@@ -162,7 +161,7 @@ function cc(card) {
         default:
             return count;
     }
-console.log("current count:", count);
+    console.log("current count:", count);
     if (count > 0) {
         return `${count} Bet`
     } else {
@@ -171,8 +170,116 @@ console.log("current count:", count);
 
     // Only change code above this line
 }
+
 cc(2);
 cc(3);
 cc(7);
 cc('K');
 cc('A');
+
+/*task 83*/
+const myDog = {
+    // Only change code below this line
+    name: "BarkBark",
+    legs: 4,
+    tails: 1,
+    friends: ["Cats", "Dogs"]
+    // Only change code above this line
+};
+
+/*task 83*/
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+/*task 84*/
+// Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+
+/*task 85*/
+// Setup
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+/*task 86*/
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.name = "Happy Coder";
+// Only change code below this line
+
+/*task 87*/
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = "woof";
+/*task 88*/
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
+/*task 89*/
+// Setup
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+    // Only change code below this line
+    const lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank",
+    };
+    result = lookup[val]
+    // Only change code above this line
+    return result;
+}
+phoneticLookup("charlie");
+/*task 90*/
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if (obj.hasOwnProperty(checkProp)){
+        return  obj[checkProp]
+    }
+    return "Not Found";
+    // Only change code above this line
+}
+/*task 91*/
