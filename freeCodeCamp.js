3293a9986bc40de522f2a52e2fbde66333b12088
Jgatzwow <thegreatgatsby1993@gmail.com>
2022-08-04 09:41:18 +0300

    Added tasks 80, 81, 82

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -105,4 +105,74 @@ function chainToSwitch(val) {
     // Only change code above this line
     return answer;
 }
+
 chainToSwitch(7);
+
+/*task 80*/
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    // Only change code above this line
+}
+
+isLess(10, 15);
+
+/*task 81*/
+
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+
+    if (a < 0 || b < 0) {
+        return;
+    }
+
+    // Only change code above this line
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
+/*task 82*/
+80
+let count = 0;
+
+function cc(card) {
+    // Only change code below this line
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+           count += 1
+            break;
+        case 7:
+        case 8:
+        case 9:
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count -= 1
+            break;
+        default:
+            return count;
+    }
+console.log("current count:", count);
+    if (count > 0) {
+        return `${count} Bet`
+    } else {
+        return `${count} Hold`
+    }
+
+    // Only change code above this line
+}
+cc(2);
+cc(3);
+cc(7);
+cc('K');
+cc('A');
