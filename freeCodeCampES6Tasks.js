a94151384cb6c99e0345ad7c8d51640a2e06b9ac
Jgatzwow <thegreatgatsby1993@gmail.com>
2022-08-04 17:52:05 +0300

    Solved Tasks till number 16;

--- freeCodeCampES6Tasks.js
+++ freeCodeCampES6Tasks.js
@@ -8,8 +8,10 @@ function checkScope() {
     console.log('Function scope i is: ', i);
     return i;
 }
+
 /*task 2*/
 const s = [5, 7, 2];
+
 function editInPlace() {
     // Only change code below this line
     s.sort();
@@ -17,5 +19,131 @@ function editInPlace() {
 
     // Only change code above this line
 }
+
 editInPlace();
+
 /*task 3*/
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+
+    // Only change code above this line
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch (ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+
+const PI = freezeObj();
+
+/*task 4*/
+const magic = () => new Date();
+/*task 5*/
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));
+/*task 6*/
+// Only change code below this line
+const increment = (number = 1, value = 1) => number + value;
+// Only change code above this line
+/*task 7*/
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+}
+/*task 8*/
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+/*task 9*/
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+const {today, tomorrow} = HIGH_TEMPERATURES;
+// Only change code above this line
+/*task 10*/
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+// Only change code above this line
+/*task 11*/
+const LOCAL_FORECAST = {
+    yesterday: {low: 61, high: 75},
+    today: {low: 64, high: 77},
+    tomorrow: {low: 68, high: 80}
+};
+
+// Only change code below this line
+
+const {today: {low: lowToday}} = LOCAL_FORECAST;
+const {today: {high: highToday}} = LOCAL_FORECAST;
+
+// Only change code above this line
+/*task 12*/
+let a = 8, b = 6;
+// Only change code below this line
+[a, b] = [b, a]
+/*task 13*/
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [a, b, ...arr] = list; // Change this line
+    // Only change code above this line
+    return arr;
+}
+
+const arr = removeFirstTwo(source);
+/*task 14*/
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+// Only change code below this line
+const half = ({max, min}) => (max + min) / 2.0;
+// Only change code above this line
+/*task 15*/
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    for (let i = 0; i < 3; i++) {
+        failureItems.push(`<li class="text-warning">${result.failure[i]}</li>`)
+    }
+    // Only change code above this line
+
+    return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+/*task 16*/
+/*task 17*/
+/*task 18*/
+/*task 19*/
+/*task 20*/
+/*task 21*/
+/*task 22*/
