f9f30b8ef11bdc73756f234546f5b6b92f9222f5
Mykhailo Sokolenko <103507912+Jgatzwow@users.noreply.github.com>
2022-08-07 12:47:36 +0300

    did tasks till 32

--- freeCodeCampRegExpTasks.js
+++ freeCodeCampRegExpTasks.js
@@ -59,17 +59,68 @@ let result = text.match(myRegex);
 /*15*/
 let reCriminals = /C+/; // Change this line
 /*16*/
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal);
 /*17*/
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
 /*18*/
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
 /*19*/
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
 /*20*/
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length;
 /*21*/
+let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/g; // Change this line
+let result = movieName.match(noNumRegex).length;
 /*22*/
+let username = "JackOfAllTrades";
+let userCheck =  /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;; // Change this line
+let result = userCheck.test(username);
 /*23*/
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
 /*24*/
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result = sample.match(countNonWhiteSpace);
 /*25*/
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6}\sno/; // Change this line
+let result = ohRegex.test(ohStr);
 /*26*/
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}\ah/; // Change this line
+let result = haRegex.test(haStr);
 /*27*/
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/; // Change this line
+let result = timRegex.test(timStr);
 /*28*/
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
 /*29*/
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6})(?=\w*\d{2})/; // Change this line
+let result = pwRegex.test(sampleWord);
 /*30*/
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor).*Roosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+/*31*/
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+)\s\1\s\1$/;
+let result = reRegex.test(repeatNum);
+/*32*/
