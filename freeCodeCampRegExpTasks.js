2ba06b8387bfcbf6c12bbe4a777e32ef5dc87f89
Mykhailo Sokolenko <103507912+Jgatzwow@users.noreply.github.com>
2022-08-07 11:18:39 +0300

    started RegExpTasks

---
+++ freeCodeCampRegExpTasks.js
@@ -0,0 +1,48 @@
+/*1*/
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString) // Change this line
+/*2*/
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+/*3*/
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+/*4*/
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
+/*5*/
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+/*6*/
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
+/*7*/
+/*8*/
+/*9*/
+/*10*/
+/*11*/
+/*12*/
+/*13*/
+/*14*/
+/*15*/
+/*16*/
+/*17*/
+/*18*/
+/*19*/
+/*20*/
+/*21*/
+/*22*/
+/*23*/
+/*24*/
+/*25*/
+/*26*/
+/*27*/
+/*28*/
+/*29*/
+/*30*/
